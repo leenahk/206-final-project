522a548a2b2e4fb52cb55faad3e81776fe0348c7
leenahk <leena@Lees-MacBook-Air-2.local>
2022-12-04 16:21:52 -0500

    fixed population data

--- population.py
+++ population.py
@@ -5,7 +5,6 @@ import csv
 
 def get_population_data():
 
-    # request data from API -- https://api.census.gov/data/timeseries/idb/1year?get=NAME,AGE,POP,AREA_KM2&GENC=&YR=2021
     x = 'FPOP65_69,FPOP70_74,FPOP75_79,FPOP80_84,FPOP85_89,FPOP90_94,FPOP95_99,MPOP65_69,MPOP70_74,MPOP75_79,MPOP80_84,MPOP85_89,MPOP90_94,MPOP95_99,'
     resp = requests.get('https://api.census.gov/data/timeseries/idb/5year?get=NAME,POP,CBR,CDR,E0,AREA_KM2,'+x+'GENC&YR=2021')
     data = resp.text
@@ -21,15 +20,14 @@ def get_population_data():
     content = file.readlines()
     file.close()
 
-
-    pop_list = []
+    pop_info = []
     
     for i in content[1:]:
         pop_dict = {}
         over_65 = 0
-
-        country = i.split(',')[0]
-        total_pop = i.split(',')[1]
+        if i.split(',')[1].isnumeric():
+            total_pop = (i.split(',')[1])
+            country = i.split(',')[0]
         j = i.split(',')[6:20]
         
         for j in i.split(',')[6:20]:
@@ -39,14 +37,14 @@ def get_population_data():
         pop_dict['over_65'] = over_65
         pop_dict['total_population'] = total_pop
         
-        pop_list.append(pop_dict)
+        pop_info.append(pop_dict)
 
-    return pop_list
+    return pop_info
 
 
 
 def main():
-   get_population_data()
+   print(get_population_data())
 
 if __name__ == "__main__":
     main()
